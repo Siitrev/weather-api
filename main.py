a17744f71e53a8d5863991ac5b6ab1ff3172e8ec
Przemyslaw Plata <paplata50@gmail.com>
2023-01-23 18:34:19 +0100

    Return json

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas
+import pandas as pd
+from datetime import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,8 +11,13 @@ def home():
 
 
 @app.route("/api/v1/<station>/<date>", methods=["GET"])
-def about(station, date):
-    return {"temp:": 25}
+def about(station: str, date: str):
+    staid = station.rjust(6, "0")
+    filepath = f"data/TG_STAID{staid}.txt"
+    df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
+
+    temperature = df[df["    DATE"] == date]["   TG"].squeeze()/10
+    return {"date:": date, "station": staid, "temperature": temperature}
 
 
 if __name__ == "__main__":
