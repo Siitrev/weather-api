5fd13840bfb5639128ff8e5edbfb9d08335fe673
Przemyslaw Plata <paplata50@gmail.com>
2023-01-23 15:31:23 +0100

    Clean-up

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pandas
 from flask import Flask, render_template
 
-app = Flask("My_app")
+app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/api/v1/<station>/<date>")
-def about(station,date):
-    return render_template("about.html")
+
+@app.route("/api/v1/<station>/<date>", methods=["GET"])
+def about(station, date):
+    return {"temp:": 25}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
